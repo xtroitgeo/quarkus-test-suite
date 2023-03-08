8ca3af543d871843cf4cd55a4fad1c9d2b8dea72
Georgii Troitskii <gtroitsk@redhat.com>
2023-03-08 10:17:59 +0100

    Add coverage for gRPC reflection service
    GrpcReflectionResponse class is used as a response type in endpoint for some gRPC reflection tests. An exception was made for protobuf file descriptors where because of serialize problems, have to return as a byte array.

--- http/http-advanced-reactive/src/main/java/io/quarkus/ts/http/advanced/reactive/GrpcResource.java
+++ http/http-advanced-reactive/src/main/java/io/quarkus/ts/http/advanced/reactive/GrpcResource.java
@@ -1,5 +1,8 @@
 package io.quarkus.ts.http.advanced.reactive;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -7,10 +10,15 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import io.grpc.reflection.v1.MutinyServerReflectionGrpc;
+import io.grpc.reflection.v1.ServerReflectionRequest;
+import io.grpc.reflection.v1.ServerReflectionResponse;
+import io.grpc.reflection.v1.ServiceResponse;
 import io.quarkus.example.Greeter;
 import io.quarkus.example.HelloReply;
 import io.quarkus.example.HelloRequest;
 import io.quarkus.grpc.GrpcClient;
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
 @Path("/grpc")
@@ -19,6 +27,50 @@ public class GrpcResource {
     @Inject
     @GrpcClient("hello")
     Greeter client;
+
+    @Inject
+    @GrpcClient("reflection-service")
+    MutinyServerReflectionGrpc.MutinyServerReflectionStub reflection;
+
+    @GET
+    @Path("reflection/service/info")
+    @Produces(MediaType.APPLICATION_JSON)
+    public GrpcReflectionResponse reflectionServiceInfo() {
+        ServerReflectionRequest request = ServerReflectionRequest.newBuilder().setHost("localhost")
+                .setListServices("").build();
+        ServerReflectionResponse response = invoke(request);
+
+        int reflectionServiceCount = response.getListServicesResponse().getServiceCount();
+        List<String> reflectionServiceList = getReflectionServiceList(response);
+
+        return new GrpcReflectionResponse(reflectionServiceCount, reflectionServiceList);
+    }
+
+    @GET
+    @Path("reflection/descriptor/{protoFilename}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public byte[] reflectionFileDescriptor(@PathParam("protoFilename") String protoFilename) {
+        ServerReflectionRequest request = ServerReflectionRequest.newBuilder()
+                .setHost("localhost").setFileByFilename(protoFilename + ".proto").build();
+
+        ServerReflectionResponse response = invoke(request);
+        return response.getFileDescriptorResponse().toByteArray();
+    }
+
+    private ServerReflectionResponse invoke(ServerReflectionRequest request) {
+        return reflection.serverReflectionInfo(Multi.createFrom().item(request))
+                .collect().first()
+                .await().indefinitely();
+    }
+
+    private List<String> getReflectionServiceList(ServerReflectionResponse response) {
+        List<ServiceResponse> serviceList = response.getListServicesResponse().getServiceList();
+        List<String> serviceNames = new ArrayList<>();
+        for (var service : serviceList) {
+            serviceNames.add(service.getName());
+        }
+        return serviceNames;
+    }
 
     @GET
     @Path("/{name}")
